7a0660e724619076072b683739481bdb3566775d
EC2 Default User <ec2-user@ip-172-31-29-77.us-east-2.compute.internal>
2018-11-08 20:37:08 +0000

    second commit

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -26,4 +26,19 @@ $(document).ready(function() {
             $("#comments").html(everything);
         })
     })
+    var url = "https://raw.githubusercontent.com/bcbooks/scriptures-json/master/book-of-mormon.json";
+    console.log(url);
+    $.getJSON(url, function(data) {
+        console.log(data);
+        var reference = '';
+        var random = Math.floor((Math.random() * 62));
+        var randomV = Math.floor((Math.random() * 9));
+        console.log("Chapter: " + random);
+        console.log("Verse: " + randomV)
+        reference += "<h1>" + data.books[8].chapters[random].verses[randomV].reference + "<br>" + "</h1>";
+        reference += "<p>" + data.books[8].chapters[random].verses[randomV].text + "<br>" + "</p>";
+        console.log(reference);
+        $("#scripture").html(reference);
+    })
+
 });
