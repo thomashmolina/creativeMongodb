83ad0e8e5f0ca27ab052f3da0e0c389def7064e4
ShinjinE <shinjinethan@hotmail.com>
2018-11-13 18:34:04 +0000

    comments.js

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             contentType: "application/json; charset=utf-8",
             success: function(data, textStatus) {
                 //$("#done").html(textStatus);
-                var name = "Name: "+$("#name").val()+"<br>"++"Comment: "+$("#comment").val();
+                var name = "Name: "+$("#name").val()+"<br>"+"Comment: "+$("#comment").val();
                 $("#json").html(name);
             }
         });
